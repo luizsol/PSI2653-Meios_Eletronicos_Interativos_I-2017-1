e29bc8b1b5b1b7345f180a7ab436efd45356bcd2
Gabriel Crabbé <gabriel.crabbe@outlook.com>
2017-05-23 20:29:19 -0300

    Adicionado código de leitura da porta
    Adicionado código de leitura da porta sob forma de protótipos de
    funções.

--- EPs/EP4/main.c
+++ EPs/EP4/main.c
@@ -72,16 +72,37 @@ int main()
 {
 	// Socket descriptor
 	int sd = socket(PF_INET, SOCK_STREAM, 0);
-	
 	if(sd == -1)
 	{
 		perror("Error getting socket descriptor");
 		exit(1);
 	}
 
+	// Parse .ini
+	FILE *ini;
+	ini = fopen("server.ini", "a+");
+	if(ini == NULL)
+	{
+		perror("Error opening server.ini");
+		exit(1);
+	}
+
+	int s_port = getPort(ini);
+	if(s_port < 0)
+	{
+		s_port = DEFAULT_PORT;
+		fprintf(ini, "port=%d\n", s_port);
+	}
+
+	char path[256];
+	if(getPath(ini, path) < 0)
+	{
+		path = "./";
+		fprintf(ini, "path=%s\n", path);
+	}
+
 	// Bind
 	int status;
-	int s_port = 5000;
 	struct sockaddr_in serveraddr;
 
 	serveraddr.sin_family      = AF_INET;
